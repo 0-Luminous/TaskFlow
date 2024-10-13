a15c7083270e222a2257f67a6f050f837f0821e7
IanNosov <g2.1.0@icloud.com>
2024-10-13 16:18:01 +0700

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Yan on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ TaskFlowTests/TaskFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowTests.swift
+//  TaskFlowTests
+//
+//  Created by Yan on 13/10/24.
+//
+
+import Testing
+@testable import TaskFlow
+
+struct TaskFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
